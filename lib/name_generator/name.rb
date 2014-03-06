ad5f92c1512a13e79c740202d7c646b115bb9417
Matt Oakley <matt@annaandmatt.net>
2014-03-06 13:40:50 +0000

    debugging with pry

--- lib/name_generator/name.rb
+++ lib/name_generator/name.rb
@@ -4,6 +4,7 @@ module NameGenerator
       options = { locale: locale }
       first_name = FirstName.new(options).generate
       last_name = LastName.new(options).generate
+      binding.pry
       if first_name && last_name then "#{first_name} #{last_name}" else "#{StandaloneName.new(options).generate}" end
     end
   end
