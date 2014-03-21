ccd732330aca675b03e76b3f8d62a254c724a8e6
Matt Oakley <matt@annaandmatt.net>
2014-03-21 14:01:12 +0000

    Allow for double barrel names.

--- lib/name_generator/last_name.rb
+++ lib/name_generator/last_name.rb
@@ -1,7 +1,17 @@
 module NameGenerator
   class LastName < Base
     def generate
-      fetch "last_name"
+      if should_be_double_barrel?
+        "#{fetch("last_name")}-#{fetch("last_name")}"
+      else
+        fetch "last_name"
+      end
+    end
+
+    private
+
+    def should_be_double_barrel?
+      rand(0..99) == 0
     end
   end
 end
